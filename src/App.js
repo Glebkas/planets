2c9fd67470216d407f041276a3d563b8f97bb029
Glebkas <baran123456@gmail.com>
2022-04-28 17:42:19 +0300

    og image

--- src/App.js
+++ src/App.js
@@ -1,38 +1,46 @@
-import React from "react";
+import React from 'react';
+import { Helmet } from 'react-helmet';
 
-import Header from "./components/Header";
-import SinglePlanet from "./components/SinglePlanet";
-import data from "./utils/data";
+import Header from './components/Header';
+import SinglePlanet from './components/SinglePlanet';
+import data from './utils/data';
 
 function App() {
-  const mercury = data[0];
-  const [planet, setPlanet] = React.useState(mercury);
-  const [open, setOpen] = React.useState(false);
+    const mercury = data[0];
+    const [planet, setPlanet] = React.useState(mercury);
+    const [open, setOpen] = React.useState(false);
 
-  function handleNavClick(planet) {
-    setPlanet(planet);
-    if (open) {
-      setOpen(false);
+    function handleNavClick(planet) {
+        setPlanet(planet);
+        if (open) {
+            setOpen(false);
+        }
     }
-  }
 
-  function handleHumburgerClick() {
-    setOpen(!open);
-  }
+    function handleHumburgerClick() {
+        setOpen(!open);
+    }
 
-  return (
-    <div className="App">
-      <div className="root">
-        <Header
-          setPlanet={setPlanet}
-          open={open}
-          handleHumburgerClick={handleHumburgerClick}
-          handleNavClick={handleNavClick}
-        />
-        <SinglePlanet planet={planet} open={open} />
-      </div>
-    </div>
-  );
+    return (
+        <div className='App'>
+            <div className='root'>
+                <Header
+                    setPlanet={setPlanet}
+                    open={open}
+                    handleHumburgerClick={handleHumburgerClick}
+                    handleNavClick={handleNavClick}
+                ></Header>
+                <Helmet>
+                    <meta
+                        property='og:image'
+                        content='https://repository-images.githubusercontent.com/444030332/fcfa2b23-4d3c-4ff1-ab31-a3cf3619785d'
+                    />
+                </Helmet>
+                <Header />
+                <SinglePlanet planet={planet} open={open} />
+            </div>
+        </div>
+    );
 }
 
 export default App;
